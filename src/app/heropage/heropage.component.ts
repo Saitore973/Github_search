b97a0b6e7481c6e5f6758fbe25e989ae271cb75a
Saitore973 <saitorevg@gmail.com>
2022-02-13 10:36:36 +0300

    add avatar, folloeing and followers to the heropage body

--- src/app/heropage/heropage.component.ts
+++ src/app/heropage/heropage.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {User} from '../user-class/user'
+import { UsersService } from '../users.service';
 @Component({
   selector: 'app-heropage',
   templateUrl: './heropage.component.html',
-  styleUrls: ['./heropage.component.css']
+  styleUrls: ['./heropage.component.css'],
 })
 export class HeropageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  user!: User;
+  constructor(private UsersService: UsersService) {}
+  searchUser(username: string) {
+    this.UsersService.getProfile(username).then(
+      (success) => {
+        this.user = this.UsersService.user;
+        console.log();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  ngOnInit(): void {
+    this.searchUser('Saitore973');
+  }
 }
+
