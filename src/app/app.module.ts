9ab03b5d9e26d8594fcda2f99013d4aaa917ef94
Saitore973 <saitorevg@gmail.com>
2022-02-13 09:44:04 +0300

    create hero page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
 import {UsersService } from './users.service';
-import { NavbarComponent } from './navbar/navbar.component'
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeropageComponent } from './heropage/heropage.component'
 @NgModule({
-  declarations: [AppComponent, ProfileComponent, NavbarComponent],
+  declarations: [AppComponent, ProfileComponent, NavbarComponent, HeropageComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [UsersService],
   bootstrap: [AppComponent],
