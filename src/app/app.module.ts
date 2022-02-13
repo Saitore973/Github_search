5e476af44e75d36495e11d8fc04a8c654eafc7e3
Saitore973 <saitorevg@gmail.com>
2022-02-13 18:17:40 +0300

    create a date count custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
 import {UsersService } from './users.service';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HeropageComponent } from './heropage/heropage.component'
+import { HeropageComponent } from './heropage/heropage.component';
+import { DateCountPipe } from './date-count.pipe';
 @NgModule({
-  declarations: [AppComponent, ProfileComponent, NavbarComponent, HeropageComponent],
+  declarations: [AppComponent, ProfileComponent, NavbarComponent, HeropageComponent, DateCountPipe],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [UsersService],
   bootstrap: [AppComponent],
