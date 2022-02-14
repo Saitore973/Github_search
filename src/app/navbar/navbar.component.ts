aa3491020ad713f0e56f7dd8fea806dd90c7220d
Saitore973 <saitorevg@gmail.com>
2022-02-14 07:07:04 +0300

    add a navigation button

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../user-class/user';
+import { UsersService } from '../users.service';
+import { Repos } from '../repos';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  user!: User;
+  repos: Repos[];
+  // username:string;
+  constructor(private UsersService: UsersService) {}
+  searchUser(username: string) {
+    this.UsersService.getProfile(username).then(
+      (success) => {
+        this.user = this.UsersService.user;
+        console.log();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  // findProfile(){
+  //   this.UsersService.getProfile(this.username);
+  // }
+
+  ngOnInit(): void {
+    this.searchUser('Saitore973');
+    this.user = this.UsersService.user;
+    this.UsersService.getRepo('Saitore973');
+    this.UsersService.getProfile('Saitore973');
+    this.repos = this.UsersService.repos;
+  }
 }
