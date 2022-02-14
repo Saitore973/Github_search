b124bb0e2cdd8dc3addd71dfd8c1e32fbb484483
Saitore973 <saitorevg@gmail.com>
2022-02-14 06:43:25 +0300

    add a router for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeropageComponent } from './heropage/heropage.component';
 const routes: Routes = [
-  // { path: 'navbar', component: NavbarComponent },
-  // { path: 'hero', component: HeropageComponent },
+  { path: 'goals', component: NavbarComponent },
+  { path: 'about', component: HeropageComponent },
 ];
 
 @NgModule({
